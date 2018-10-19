60ca95a585da195f05985ec6a93a588bf3dfc3e1
CiaranGruber <ciaran.gruber@my.jcu.edu.au>
2018-10-19 21:22:01 +0100

    Complete temperature task using Flask

--- greet/app.py
+++ greet/app.py
@@ -14,5 +14,20 @@ def greet(name=""):
     return 'Hello ' + name
 
 
+@app.route('/<celsius>')
+def show_celsius(celsius=""):
+    try:
+        string = 'Original value: ' + celsius + '<br />'
+        string += 'Converted value: ' + str(convert_to_fahrenheit(int(celsius)))
+    except ValueError:
+        string = 'The value entered was not a number'
+    return string
+
+
+def convert_to_fahrenheit(celsius):
+    """Convert a value from Celsius to Fahrenheit"""
+    return celsius * 9.0 / 5 + 32
+
+
 if __name__ == '__main__':
     app.run()
